4bca55bf8f47ffe1f23d47b7de1ae98264200913
Bekarys <bsailaukul@gmail.com>
2020-03-18 00:32:11 +0600

    lab7

--- todolist/main.js
+++ todolist/main.js
@@ -5,7 +5,6 @@
         checkbox.type = "checkbox";
         var li = document.createElement('li');
         var label = document.createElement('label')
-        label.htmlFor = "id";
         label.marginLeft = "30pt";
         label.innerHTML=delo;
         li.appendChild(checkbox);
@@ -14,22 +13,24 @@
         document.getElementById('in').value="";
 
         var btn = document.createElement('button');
-    btn.className = 'btn';
-    btn.innerHTML = '<i class="fas fa-trash"></i>';
-    btn.style.height = "20pt";
-    btn.style.width = "20pt";
-    btn.style.float = "right";
-    btn.style.padding = "0"
-    li.appendChild(btn);
-
-    btn.onclick = function(){
-        li.removeChild(checkbox);
-        li.removeChild(label);
-        li.removeChild(btn);
-        li.parentNode.removeChild(li);
-    }
-
-    checkbox.onclick = function(){
+        btn.className = 'btn';
+        btn.innerHTML = '<i class="fas fa-trash"></i>';
+        btn.style.height = "20pt";
+        btn.style.width = "20pt";
+        btn.style.float = "right";
+        btn.style.padding = "0"
+        li.appendChild(btn);
+        
+        btn.onclick = function(){
+           /* li.removeChild(checkbox);
+            li.removeChild(label);
+            li.removeChild(btn);
+            li.parentNode.removeChild(li);*/
+            UL = document.getElementById("myUl");
+            UL.removeChild(li);
+        }
+    
+        checkbox.onclick = function(){
         if (checkbox.checked == true){
             label.style.textDecoration = "line-through";
         } else {
